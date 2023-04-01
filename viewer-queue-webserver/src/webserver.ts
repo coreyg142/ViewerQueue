ba89227d9f869756a17d00d3aee37a3b99550048
Corey G <coreyg142@gmail.com>
2023-04-01 00:05:27 -0400

    started on routes

--- viewer-queue-webserver/src/webserver.ts
+++ viewer-queue-webserver/src/webserver.ts
@@ -1,7 +1,13 @@
 import express from "express";
 
+import { notFound } from "./routes/index.js";
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get("*", (req, res) => {
+  notFound(req, res);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
