00ce7faea02c15ac6530dcf632720967babdf0db
Corey G <coreyg142@gmail.com>
2023-04-01 23:28:06 -0400

    renaming files

--- viewer-queue-webserver/src/routes/index.ts
+++ viewer-queue-webserver/src/routes/index.ts
@@ -1,6 +1,6 @@
 export { default as notFound } from "./notFound.js";
-export { default as addName } from "./addName.js";
+export { default as addName } from "./add.js";
 // export { default as deleteName } from "./deleteName.js";
-export { default as popName } from "./popName.js";
+export { default as popName } from "./pop.js";
 // export { default as reOrderName } from "./reOrderName.js";
-export { default as getQueue } from "./getQueue.js";
+export { default as getQueue } from "./get.js";
