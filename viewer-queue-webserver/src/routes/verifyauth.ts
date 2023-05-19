86b02aa7ffd0e8a4cf155622e55cb7068e1a3578
Corey G <coreyg142@gmail.com>
2023-05-18 23:49:07 -0400

    Fixed bug in verifyauth.ts

--- viewer-queue-webserver/src/routes/verifyauth.ts
+++ viewer-queue-webserver/src/routes/verifyauth.ts
@@ -6,15 +6,19 @@ import { Request, Response } from "express";
 
 export default async function verifyAuth(req: Request, res: Response) {
   const key = req.headers?.api_auth;
-  console.log(req.headers);
+  // console.log(req.headers);
+  const ip = req.headers?.["cf-connecting-ip"] || req.socket.remoteAddress;
+
   const verified =
-    typeof key === "string"
-      ? AuthKeyManager.verifyKey(key, req.ip)
+    typeof key === "string" && typeof ip === "string"
+      ? AuthKeyManager.verifyKey(key, ip)
       : { valid: false, expiryTimeMs: -1, message: "Weirdness" };
-  console.log(verified.message);
+  // console.log(verified.message);
   if (verified.valid) {
+    console.log(`Verified auth key for ${ip}`);
     res.status(200).json({ result: "valid", expiryTimeMs: verified.expiryTimeMs });
   } else {
+    console.log(`Failed to verify auth key for ${ip}`);
     res.status(200).json({ error: "Unauthorized" });
     return;
   }
